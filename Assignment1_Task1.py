d95bca970dbd1c98750ad21e0e563cc44e10e5d7
Abhay Pande <abhaypande26@gmail.com>
2025-07-05 09:38:01 +0530

    Assignment 1_Task 1 & 2

---
+++ Assignment1_Task1.py
@@ -0,0 +1,9 @@
+a=input("Enter the first number: ")
+b=input("Enter the second number: ")
+a=int(a)
+b=int(b)
+
+print("Addition: ", a+b)
+print("Substraction: ", a-b)
+print("Multiplication: ", a*b)
+print("Division: ", a/b)
