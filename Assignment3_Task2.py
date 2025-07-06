839a2383dc3ea0fae4b89de74f2af020908ddeea
Abhay Pande <abhaypande26@gmail.com>
2025-07-06 07:35:15 +0530

    Assignment 2_Task 1 & 2

---
+++ Assignment3_Task2.py
@@ -0,0 +1,8 @@
+import math
+
+
+num= input("Enter a number: ")
+
+print("Square Root: "+ str(math.sqrt(int(num))))
+print("Logarithm: "+ str(math.log(int(num))))
+print("Sine: "+ str(math.sin(int(num))))
